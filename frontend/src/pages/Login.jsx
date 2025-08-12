2198c96b936a18b3f90d2d127f52e1f17be7fc38
Spoorthi1411 <spoorthikoutam14@gmail.com>
2025-08-12 20:21:35 +0530

    login and logout

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,13 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import {AppContext} from '../context/AppContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
 
+  const {backendUrl,token,setToken}=useContext(AppContext)
+  const navigate = useNavigate()
   const [state,setState] = useState('Sign Up')
   const [email,setEmail] = useState('')
   const [password,setPassword] =useState('')
@@ -9,10 +15,39 @@ const Login = () => {
 
   const onSubmitHandler = async (event) => {
     event.preventDefault()
+
+    try {
+      if(state==='Sign Up'){
+        const {data} = await axios.post(backendUrl+ '/api/user/register',{name,password,email})
+        if(data.success){
+          localStorage.setItem('token',data.token)
+          setToken(data.token)
+        }else{
+          toast.error(data.message)
+        }
+      }
+      else{
+        const {data} = await axios.post(backendUrl+'/api/user/login',{password,email})
+        if(data.success){
+          localStorage.setItem('token',data.token)
+          setToken(data.token)
+        }else{
+          toast.error(data.message)
+        }
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
   }  
 
+  useEffect(()=>{
+    if(token){
+      navigate('/')
+    }
+  },[token])
+
   return (
-    <form className='min-h-[80vh] flex items-center'>
+    <form onSubmit={onSubmitHandler} className='min-h-[80vh] flex items-center'>
       <div className='flex flex-col gap-3 m-auto items-start p-8 min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-xl'>
         <div className='text-center w-full'>
           <p className='text-3xl font-semibold mb-2'>{state === 'Sign Up' ? "Create Account" : "Login"}</p>
@@ -32,7 +67,7 @@ const Login = () => {
           <p>Password</p>
           <input className='border border-zinc-300 rounded w-full p-2 mt-1' type='password' onChange={(e)=>setPassword(e.target.value)} value={password}/>
         </div>
-        <button className='bg-[#db6d7c] text-white py-2 rounded-md text-base w-full '>{state === 'Sign Up' ? "Create Account" : "Login"}</button>
+        <button type='submit' className='bg-[#db6d7c] text-white py-2 rounded-md text-base w-full '>{state === 'Sign Up' ? "Create Account" : "Login"}</button>
         {
           state === 'Sign Up'
           ? <p>Already have an account? <span onClick={()=>setState('Login')} className='text-[#db6d7c] underline cursor-pointer'>Login here</span></p>
